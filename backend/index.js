f09656a643ed8945c9f22ecac91ee166a6969a33
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-01-30 17:40:29 -0300

    Connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const validator = require("express-validator");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
+
+//Connect to MongoDB
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect(process.env.MONGODB);
+}
+
+//Allow CORS
+const allowedOrigins = ["http://localhost:5173"];
+const options = {
+    origin: allowedOrigins
+};
+app.use(cors(options));
+
+//Use JSON parser
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hola");
